a9ac0afa184bc8a111b897bf3972664fc1677f61
cristian <crispisjam@gmail.com>
2021-06-05 00:46:14 -0500

    se cambio todo el inicio, se diseño y maqueto un portafolio

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,22 +3,18 @@ import ExtLink from './ext-link'
 export default function Footer() {
   return (
     <>
-      <footer>
-        <span>Deploy your own!</span>
-        <ExtLink href="https://vercel.com/new/git/external?repository-url=https://github.com/ijjk/notion-blog/tree/main&project-name=notion-blog&repository-name=notion-blog">
-          <img
-            src="https://vercel.com/button"
-            height={46}
-            width={132}
-            alt="deploy to Vercel button"
-          />
-        </ExtLink>
-        <span>
-          or{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog">
-            view source
-          </ExtLink>
-        </span>
+      <footer className="explanation">
+        <img className="logo-blanco" src="/logo-blanco.svg" alt="logo" />
+        <div className="seccion-redes">
+          <p>
+            Hecho con &#128153; por Cristian. Copyright 2021 - todos los
+            derechos reservados.
+          </p>
+          <img src="/telefono.svg" alt="telefono" />
+          <img src="/correo.svg" alt="correo" />
+          <img src="/instagram.svg" alt="instagram" />
+          <img src="/twitter.svg" alt="twitter" />
+        </div>
       </footer>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,152 @@
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
-import Features from '../components/features'
 import sharedStyles from '../styles/shared.module.css'
 
 export default function Index() {
   return (
     <>
       <Header titlePre="Home" />
-      <div className={sharedStyles.layout}>
-        <img
-          src="/vercel-and-notion.png"
-          height="85"
-          width="250"
-          alt="Vercel + Notion"
-        />
-        <h1>My Notion Blog</h1>
-        <h2>
-          Blazing Fast Notion Blog with Next.js'{' '}
-          <ExtLink
-            href="https://github.com/vercel/next.js/issues/9524"
-            className="dotted"
-            style={{ color: 'inherit' }}
-          >
-            SSG
+      <div className="explanation usuario">
+        <div className="presentacion-usuario">
+          <h5>¡Hola a todos!</h5>
+          <h2>Soy Cristian</h2>
+          <p className="intro">
+            Full Stack Developer que le encanta implementar diseños que inspiran
+            y atraen a las personas, ademas de esto de crear software que cambie
+            y mejore vidas a traves de tecnología.
+          </p>
+          <div className="seccion-redes">
+            <img src="/telefono.svg" alt="telefono" />
+            <img src="/correo.svg" alt="correo" />
+            <img src="/instagram.svg" alt="instagram" />
+            <img src="/twitter.svg" alt="twitter" />
+          </div>
+          <div className="curriculum">
+            <ExtLink className="boton">DESCARGAR CURRICULUM</ExtLink>
+          </div>
+        </div>
+        <div>
+          <img src="/user.png" alt="" />
+        </div>
+      </div>
+      <div className="explanation carrera">
+        <p className="intro-carrera">
+          A lo largo de mi carrera como Frontend, he tenido el privilegio de
+          trabajar en proyectos retadores e increíbles
+        </p>
+        <h5>Aquí hay algunos que me gustaría compartir.</h5>
+        <img src="flecha-abajo.png" alt="flecha abajo" />
+      </div>
+      <div id="proyectos" className="explanation portafolio">
+        <div className="info-proyecto">
+          <h4 className="titulo-proyecto">empleosdeleje.com</h4>
+          <p>
+            Accede a ofertas laborales en el eje cafetero, además de ofrecerte
+            servicios para mejorar tu hoja de vida y tus entrevistas laborales
+          </p>
+          <ExtLink href="https://empleosdeleje.com" className="ver-proyecto">
+            VER PROYECTO
           </ExtLink>
-        </h2>
-
-        <Features />
-
-        <div className="explanation">
+        </div>
+        <div className="imagen-proyecto">
+          <img src="/mockup-web-empleos-del-eje.png" alt="empleos del eje" />
+        </div>
+      </div>
+      <div className="explanation recomendaciones">
+        <img src="/leonidas.png" alt="leonidas" />
+        <p className="texto-recomendacion">
+          "Jaume ha sido de gran ayuda para acelerar ciertos esfuerzos de
+          desarrollo. Lo que nos hubiera llevado 6 meses solo les llevó 1,5
+          meses ".
+        </p>
+        <h5 className="nombre-recomendacion">
+          LEONIDAS ESTEBAN / COFUNDADOR Y CEO LEONIDASESTEBAN.COM
+        </h5>
+      </div>
+      <div id="proyectos" className="explanation portafolio">
+        <div className="imagen-proyecto">
+          <img src="/tablet-top-travel.png" alt="top-travel" />
+        </div>
+        <div className="info-proyecto">
+          <h4 className="titulo-proyecto">toptravel.com.co</h4>
           <p>
-            This is a statically generated{' '}
-            <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-            <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-            is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>
-            . It leverages some upcoming features in Next.js like{' '}
-            <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-              SSG support
-            </ExtLink>{' '}
-            and{' '}
-            <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-              built-in CSS support
-            </ExtLink>{' '}
-            which allow us to achieve all of the benefits listed above including
-            blazing fast speeds, great local editing experience, and always
-            being available!
+            Página web de agencia de viajes, donde los clientes pueden ver los
+            vuelos disponibles, precios y poder hacer su reserva en linea
           </p>
-
+          <ExtLink href="https://toptrevel.com.co" className="ver-proyecto">
+            VER PROYECTO
+          </ExtLink>
+        </div>
+      </div>
+      <div className="explanation recomendaciones">
+        <img src="/usuario-2.png" alt="leonidas" />
+        <p className="texto-recomendacion">
+          "Jaume ha superado mis expectativas desde el diseño hasta el
+          desarrollo".
+        </p>
+        <h5 className="nombre-recomendacion">
+          JASON LOPEZ / COFUNDADOR Y CEO MARVOL
+        </h5>
+      </div>
+      <div id="proyectos" className="explanation portafolio">
+        <div className="info-proyecto margin-app">
+          <h4 className="titulo-proyecto">Delivery Taste</h4>
           <p>
-            Get started by creating a new page in Notion and clicking the deploy
-            button below. After you supply your token and the blog index id (the
-            page's id in Notion) we will automatically create the table for you!
-            See{' '}
-            <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-              here in the readme
-            </ExtLink>{' '}
-            for finding the new page's id. To get your token from Notion, login
-            and look for a cookie under www.notion.so with the name `token_v2`.
-            After finding your token and your blog's page id you should be good
-            to go!
+            Web app de delivery hecha en Svelte, en ella se puede agregar los
+            productos al carrito, añadir tus adiciones y bebidas preferidas y
+            por supuesto finalizar tu compra.
           </p>
+          <div className="botones">
+            <ExtLink
+              href="https://cranky-bose-fac8af.netlify.app/"
+              className="ver-proyecto"
+            >
+              VER PROYECTO
+            </ExtLink>
+            <ExtLink
+              href="https://cranky-bose-fac8af.netlify.app/"
+              className="ver-codigo"
+            >
+              VER CODIGO
+            </ExtLink>
+          </div>
+        </div>
+        <div className="imagen-proyecto margin-app">
+          <img src="/delivery-app.png" alt="app de delivery" />
+        </div>
+      </div>
+      <div className="explanation recomendaciones">
+        <img src="/usuario-3.png" alt="leonidas" />
+        <p className="texto-recomendacion">
+          "Por primera vez en la historia de nuestra empresa, aparecimos en
+          todas las plataformas en las que trabajamos".
+        </p>
+        <h5 className="nombre-recomendacion">MARCO CORTEZ / NETNET</h5>
+      </div>
+      <div className="explanation hablemos">
+        <div className="formulario">
+          <h4 className="titulo-proyecto center">HABLEMOS</h4>
+          <p className="texto-form center">
+            Si está interesado en trabajar conmigo en su próximo proyecto, no
+            dude en ponerse en contacto conmigo.
+          </p>
+          <form action="">
+            <div className="nombre">
+              <label>Nombre completo</label>
+              <input type="text" placeholder="Cristian" />
+            </div>
+            <div className="correo">
+              <label>Correo electrónico</label>
+              <input type="text" placeholder="cristian@gmail.com" />
+            </div>
+            <div className="mensaje">
+              <label>Mensaje</label>
+              <textarea name="" id=""></textarea>
+            </div>
+            <div className="enviar">
+              <button>ENVIAR MENSAJE</button>
+            </div>
+          </form>
         </div>
       </div>
     </>
